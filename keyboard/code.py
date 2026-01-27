f12cc65d985ac3740af258bb07e55cb2aecea98d
xloqqq <xloqqq@eclipso.eu>
2026-01-27 06:10:44 +0100

    MacCyrillic support
    minor font fixes
    
    more minor font fixes
    
    python syntax fixes
    
    ligature support

--- keyboard/code.py
+++ keyboard/code.py
@@ -55,9 +55,8 @@ FONT = {
     ord("H"): 0b1110100,
     ord("I"): 0b0000100,
     ord("J"): 0b0011110,
-    ord("K"): 0b1110110,
+    ord("K"): 0b1110101,
     ord("L"): 0b0111000,
-    ord("M"): 0b0110111,
     ord("N"): 0b1010100,
     ord("O"): 0b1011100,
     ord("P"): 0b1110011,
@@ -67,12 +66,126 @@ FONT = {
     ord("T"): 0b1111000,
     ord("U"): 0b0011100,
     ord("V"): 0b0111110,
-    ord("W"): 0b1111110,
     ord("X"): 0b1110110,
     ord("Y"): 0b1101110,
     ord("Z"): 0b0011011,
     ord("-"): 0b1000000,
     ord("_"): 0b0001000,
+    ord("\""): 0b0100010,
+    ord("!"): 0b1101011,
+    ord("?"): 0b1010011,
+    ord("="): 0b1001000,
+    ord("^"): 0b0100011,
+    ord("("): 0b0111001,
+    ord(")"): 0b0001111,
+    ord(":"): 0b0001001,
+    ord(";"): 0b0000101,
+    ord("["): 0b0111001,
+    ord("]"): 0b0001111,
+    ord("{"): 0b1000110,
+    ord("}"): 0b1110000,
+    ord("<"): 0b1100001,
+    ord(">"): 0b1000011,
+    ord("/"): 0b1010010,
+    ord("\\"): 0b1100100,
+    ord("~"): 0b1010010,
+    ord("@"): 0b1111011,
+    ord("`"): 0b1100000,
+    # MacCyrillic (тому що можу!)
+    0x80: 0b1110111, # 'А'
+    0x81: 0b1111101, # 'Б'
+    0x82: 0b1111111, # 'В'
+    0x83: 0b0110011, # 'Г'
+    0x84: 0b1101111, # 'Д'
+    0x85: 0b1111001, # 'Е'
+    0x87: 0b0001111, # 'З'
+    0x88: 0b0011100, # 'И'
+    0x89: 0b0011101, # 'Й'
+    0x8A: 0b1110101, # 'К'
+    0x8B: 0b1010110, # 'Л'
+    0x8C: 0b0100111, # 'М' (right part)
+    0x8D: 0b1110110, # 'Н'
+    0x8E: 0b1011100, # 'О'
+    0x8F: 0b0110111, # 'П'
+    0x90: 0b1110011, # 'Р'
+    0x91: 0b0111001, # 'С'
+    0x92: 0b0110001, # 'Т' (right part)
+    0x93: 0b1101110, # 'У'
+    0x96: 0b1101010, # 'Ц'
+    0x97: 0b1100110, # 'Ч'
+    0x98: 0b0111100, # 'Ш' (left part)
+    0x99: 0b1100010, # 'Щ' (left part)
+    0x9A: 0b0000001, # 'Ъ' (left part)
+    0x9C: 0b1111100, # 'Ь'
+    0x9D: 0b1001111, # 'Э'
+    0x9F: 0b1010111, # 'Я'
+    0xDF: 0b1010111, # 'я'
+    0xA1: 0b1100011, # '˚'
+    0xA2: 0b1010010, # 'Ґ'
+    0xB6: 0b1010010, # 'ґ'
+    0xA6: 0b1100111, # '¶'
+    0xA7: 0b0110000, # 'І'
+    0xB4: 0b0110000, # 'і'
+    0xAB: 0b1100101, # 'Ђ'
+    0xAC: 0b1100101, # 'ђ'
+    0xCB: 0b1110101, # 'Ћ'
+    0xCC: 0b1110101, # 'ћ'
+    0xAE: 0b1010001, # 'Ѓ'
+    0xAF: 0b1010001, # 'ѓ'
+    0xB2: 0b1101001, # '≤'
+    0xB3: 0b1001011, # '≥'
+    0xB5: 0b1110010, # 'µ'
+    0xB7: 0b0001110, # 'Ј'
+    0xC0: 0b0001110, # 'ј'
+    0xB8: 0b1111001, # 'Є'
+    0xB9: 0b1111001, # 'є'
+    0xBA: 0b0010001, # 'Ї'
+    0xBB: 0b0010001, # 'ї'
+    0xC1: 0b1101101, # 'Ѕ'
+    0xCF: 0b1101101, # 'ѕ'
+    0xC2: 0b0000011, # '¬'
+    0xC3: 0b0011110, # '√'
+    0xCD: 0b1011001, # 'Ќ'
+    0xCE: 0b1011001, # 'ќ'
+    0xD2: 0b0100010, # '“'
+    0xD3: 0b0100010, # '”'
+    0xD4: 0b0000010, # '‘'
+    0xD5: 0b0100000, # '’'
+    0xD7: 0b0010100, # '„'
+    0xD8: 0b1001101, # 'Ў'
+    0xD9: 0b1001101, # 'ў'
+    0xDA: 0b1101010, # 'Џ'
+    0xDB: 0b1101010, # 'џ'
+    0xDD: 0b1011001, # 'Ё'
+    0xDE: 0b1011001, # 'ё'
+    0xFF: 0b1111001, # '€'
+}
+
+REPLACE = {
+    ord("%"): (0xA1, ord("O")), # % → °o
+    ord("+"): (ord("{"), ord("}")), # + → {}
+    ord("M"): (0x83, 0x8C), # M → ГМ
+    ord("W"): (0x98, 0xC3), # W → Ш√
+    0x86: (0x9D, 0xB8), # Ж → ЭЄ
+    0x8C: (0x83, 0x8C), # М → ГМ
+    0x92: (ord("7"), 0x92), # Т → 7Т
+    0x94: (0xA6, 0x90), # Ф → ¶Р
+    0x95: (ord("]"), ord("[")), # Х → ][
+    0x98: (0x98, 0xC3), # Ш → Ш√
+    0x99: (0x99, 0x97), # Щ → ЩЧ
+    0x9A: (0x9A, 0x9C), # Ъ → ЪЬ
+    0x9B: (0x9C, ord("1")), # Ы → Ь1
+    0x9E: (ord("}"), ord("0")), # Ю → }0
+    0xA8: (ord("("), ord("R"), ord(")")), # ® → (R)
+    0xA9: (ord("("), ord("C"), ord(")")), # © → (C)
+    0xB0: (ord("O"), ord("O")), # ∞ → oo
+    0xBC: (0x92, 0x9C), # Љ → ТЬ
+    0xBD: (0x92, 0x9C), # љ → ТЬ
+    0xBE: (ord("}"), 0x9C), # Њ → }Ь
+    0xBF: (ord("}"), 0x9C), # њ → }Ь
+    0xD0: (ord("-"), ord("-")), # — → --
+    0xD1: (ord("-"), ord("-"), ord("-")), # – → ---
+    0xDC: (ord("n"), ord("o")), # № → no
 }
 
 # Map symbol to segment and digit.
@@ -163,7 +276,7 @@ usb_keyboard = Keyboard(usb_hid.devices)
 
 display_buffer_offset = 0
 display_buffer_len = 0
-display_buffer_capacity = 48
+display_buffer_capacity = 48 * 3
 display_buffer = [255 for _ in range(display_buffer_capacity)]
 
 scroll_last_time = time.monotonic()
@@ -229,9 +342,6 @@ def display_segments_byte(position: int, segments_byte: int):
     write_digit(position, True)
 
 def display_char(position: int, char: int):    # Convert lowercase to uppercase.
-    if 97 <= char and char <= 122:
-        char = char - 32
-
     # All unknown characters will be replaced with spaces.
     display_segments_byte(position, FONT.get(char, 0))
 
@@ -305,8 +415,16 @@ def usb_hid_poll_reports():
             if byte == 0:
                 break
 
-            display_buffer[display_buffer_len] = byte
-            display_buffer_len += 1
+            if 97 <= byte and byte <= 122:
+                byte = byte - 32
+            if 0xE0 <= byte and byte <= 0xFE:
+                byte = byte - 0x60
+
+
+            ligature = REPLACE.get(byte, (byte,))
+            for glyph in ligature:
+                display_buffer[display_buffer_len] = glyph
+                display_buffer_len += 1
         
         if display_buffer_len > 0:
             # Hide time and reset scroll timer (with small delay at first letter)
